b1ea0d27b06e18dea5925d987f44f8ddd33393a9
Tam Lam <imtamtlam@gmail.com>
2019-02-10 17:07:16 +0700

    Fix quitall: for all bash apps

--- source/lazy-mac.py
+++ source/lazy-mac.py
@@ -2,8 +2,8 @@
 import time, os , sys, subprocess
 import threading
 import argparse
+from subprocess import Popen, PIPE
 response = None
-
 # Commands
 sleepCommand = "sleep"
 quitAllCommand = "quitall"
@@ -21,53 +21,42 @@ tell application "Safari"
     end repeat
 end tell
 tell application "Safari" to quit
-
 '''
 closeChromeTabsScript = '''
-tell application "Google Chrome"
-	repeat with i from (count of windows) to 1 by -1
-        repeat with j from (count of tabs of window i) to 1 by -1
-            set thistab to tab j of window i
-            set foo to name of thistab
-            if foo is not equal to "bar" then close thistab
+if exists application "Google Chrome" then
+    tell application "Google Chrome"
+        repeat with i from (count of windows) to 1 by -1
+            repeat with j from (count of tabs of window i) to 1 by -1
+                set thistab to tab j of window i
+                set foo to name of thistab
+                if foo is not equal to "bar" then close thistab
+            end repeat
         end repeat
-    end repeat
-end tell
-tell application "Google Chrome" to quit
-
+    end tell
+    tell application "Google Chrome" to quit
+end if
 '''
-quitAllScript='''
+getBashAppNameScript = '''
 tell application "System Events"
-    set selectedProcesses to (name of every process where background only is false)
+    set terminalApp to name of first application process whose frontmost is true
+    get terminalApp
 end tell
-repeat with processName in selectedProcesses
-    if processName does not contains "Terminal"
-        do shell script "Killall " & quoted form of processName
-    end if
-end repeat
-tell application "Terminal" to quit
 '''
 shutdownScript ='''
 tell application "System Events"
 shut down
 end tell
 '''
-
-sleepScript = '''
-tell application "Finder" to sleep
-'''
-
 #Parser
 parser = argparse.ArgumentParser(description= "Timer to make Mac falls asleep")
 commands = parser.add_mutually_exclusive_group()
 commands.add_argument('-sleep','--sleep', metavar = '', type=int, help='Sleep in number of minutes')
 commands.add_argument('-quitall','--quitall', metavar = '', type=int, help='Quit all applications in shutdown in number of minutes')
 commands.add_argument('-shutdown','--shutdown', metavar = '', type=int, help='Shutdown in number of minutes')
-
 args = parser.parse_args()
 thread1 = threading.Thread()
 
-def timer(seconds,command):    
+def timer(seconds,command):
     while True:
         try: 
             timer = abs(seconds)
@@ -98,6 +87,12 @@ def performCommand(command):
             quitall()
         if command == shutdownCommand:
             shutdown()
+
+def getBashAppName():
+    proc = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    name, error = proc.communicate(getBashAppNameScript)
+    name = str(name).rstrip()
+    return name
 
 def quit():
     while True:
@@ -151,8 +146,25 @@ def executeApplescript(applescript):
     proc.wait()
     return proc
 
-
 if __name__ == "__main__":
+    bashAppName = getBashAppName()
+    quitAllScript='''
+    tell application "System Events"
+        set selectedProcesses to (name of every process where background only is false)
+    end tell
+    repeat with processName in selectedProcesses
+        if processName does not contains "%s"
+            do shell script "Killall " & quoted form of processName
+        end if
+    end repeat
+    do shell script "Killall " & quoted form of "%s"
+    ''' % (bashAppName,bashAppName)
+
+    sleepScript = '''
+    tell application "Finder" to sleep
+    do shell script "Killall " & quoted form of "%s"
+    ''' % (bashAppName)
+
     if args.sleep:
         argsSwitch(args.sleep , sleepCommand)
     if args.shutdown:
@@ -161,4 +173,3 @@ if __name__ == "__main__":
         argsSwitch(args.quitall, quitAllCommand)
     else: 
         print("Unknown argument\nEnter \"lazy-mac -h\" to view list of arguments ")
-pass
